bbc818b3a20a394735c4ee3fcce36b579523956a
sivamani-muraboyina <sivamanim837@gmail.com>
2026-03-26 02:19:24 +0530

    Created a basic streamlit app for prediction

---
+++ app/app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import requests
+
+st.title("Loan Eligibility Prediction")
+
+# Inputs
+gender = st.selectbox("Gender", ["Male", "Female"])
+married = st.selectbox("Married", ["Yes", "No"])
+dependents = st.selectbox("Dependents", ["0", "1", "2", "3+"])
+education = st.selectbox("Education", ["Graduate", "Not Graduate"])
+self_employed = st.selectbox("Self Employed", ["Yes", "No"])
+
+app_income = st.number_input("Applicant Income", min_value=0)
+co_income = st.number_input("Coapplicant Income", min_value=0)
+loan_amount = st.number_input("Loan Amount", min_value=0)
+
+loan_term = st.selectbox("Loan Term", [12, 36, 60, 120, 180, 240, 300, 360, 480])
+
+credit = st.selectbox("Credit History", [1.0, 0.0])
+property_area = st.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
+
+# Button
+if st.button("Predict"):
+
+    data = {
+        "Gender": gender,
+        "Married": married,
+        "Dependents": dependents,
+        "Education": education,
+        "Self_Employed": self_employed,
+        "ApplicantIncome": app_income,
+        "CoapplicantIncome": co_income,
+        "LoanAmount": loan_amount,
+        "Loan_Amount_Term": loan_term,
+        "Credit_History": credit,
+        "Property_Area": property_area
+    }
+
+    try:
+        response = requests.post("http://127.0.0.1:8000/predict", json=data)
+
+        if response.status_code == 200:
+            result = response.json()
+            st.success(f"Prediction: {result['prediction']}")
+        else:
+            st.error("Error from API")
+
+    except Exception as e:
+        st.error("API not running. Start FastAPI server.")
