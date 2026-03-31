df47c5091880996d47026c3b3f974aa13bb30266
sivamani-muraboyina <sivamanim837@gmail.com>
2026-03-31 09:15:46 +0530

    created logging and customexception

---
+++ src/exception.py
@@ -0,0 +1,16 @@
+import sys
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = self.get_error_message(error_message, error_detail)
+
+    def get_error_message(self, error_message, error_detail: sys):
+        _, _, exc_tb = error_detail.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+        return f"Error in {file_name}, line {line_number}: {error_message}"
+
+    def __str__(self):
+        return self.error_message
